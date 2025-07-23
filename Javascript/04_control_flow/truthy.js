4ee5e72b76e867bacd6c0078c20175e376b1d049
Sachin Shakya <sachinshakya613@gmail.com>
2025-07-23 16:16:21 +0530

    learned Nullish Coalscring Operator

--- Javascript/04_control_flow/truthy.js
+++ Javascript/04_control_flow/truthy.js
@@ -27,3 +27,40 @@ if(Object.keys(emptyObj).length === 0){
 }
 
 
+// Nullish Coalscing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10;
+val1 = null ?? 10; 
+val1 = undefined ?? 15; 
+val1 = null ?? 10 ?? 15
+
+// why use this operator sometimes when you database or using firebase
+// then maybe you donot get response directly
+//you get two values null or chances maybe you dont get or undefined
+
+
+console.log(val1);
+
+
+//Ternary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("More than 80")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
