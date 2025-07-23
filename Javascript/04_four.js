bf7c36f8eb6404c2a4ea1f4c4eda8ea1da65c916
Sachin Shakya <sachinshakya613@gmail.com>
2025-07-23 19:59:14 +0530

    done with forin loop

--- Javascript/04_four.js
+++ Javascript/04_four.js
@@ -6,11 +6,39 @@ const myObject = {
 }
 
 for (const key in myObject){
+    //it shows only key 
     console.log(key);
 }
 for (const key in myObject){
+    //it shows only values
     console.log(myObject[key]);
 }
 for (const key in myObject){
+    //it shows both
     console.log(`${key} -> ${myObject[key]}`);
+}
+
+// let see forin loop work on array or not
+const programming = ["js","rb","py","java","cpp"]
+
+for (const key in programming) {
+  console.log(key);
+}
+for (const key in programming) {
+  console.log(programming[key]);
+}
+for (const key in programming) {
+  console.log(`${key}->${programming[key]}`);
+}
+
+// now let see will it work on map or not
+
+const map = new Map()
+map.set('IN','India')
+map.set('USA','United States of America')
+map.set('Fr','France')
+map.set('Fr','France')
+
+for (const key in map) {
+    console.log(key);
 }
